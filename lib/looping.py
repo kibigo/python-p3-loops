c1924889fed890441d12f0104fd7ba9c9c834572
kibigo <jacksonkibigo99@gmail.com>
2023-09-29 22:46:16 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 11
+    while counter > 1:
+        counter -=1
+        print(counter)
+        if counter == 1:
+            print("Happy New Year!")
+        
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_items = [integers ** 2 for integers in int_list]
+    return (squared_items)
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+
+    for num in range(1, 101):
+        if (num % 3 == 0 and num % 5 == 0):
+            print("FizzBuzz")
+        elif (num % 3 == 0):
+            print("Fizz")
+        elif(num % 5 == 0):
+            print("Buzz")
+        else:
+            print(num)
+    
